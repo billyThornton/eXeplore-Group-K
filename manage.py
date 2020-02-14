144b21110481175edd018b0b241afc9205fd4b53
Billy Thornton <billy.bob.thornton@sky.com>
2020-02-14 10:27:43 +0000

    changed manage.py

--- manage.py
+++ manage.py
@@ -24,6 +24,11 @@ def root():
 # *     "name": "Bob"
 # * }
 # */
+
+@app.route('/dashboard')
+def dashboard():
+    return app.send_static_file('Game_Keeper_Page.html')
+    
 @app.route('/api/visitors', methods=['GET'])
 def get_visitor():
     if client:
@@ -31,36 +36,6 @@ def get_visitor():
     else:
         print('No database')
         return jsonify([])
-
-# /**
-#  * Endpoint to get a JSON array of all the visitors in the database
-#  * REST API example:
-#  * <code>
-#  * GET http://localhost:8000/api/visitors
-#  * </code>from flask import Flask, render_template, request, jsonify
-import atexit
-import os
-import json
-
-app = Flask(__name__, static_url_path='')
-
-db_name = 'mydb'
-client = None
-db = None
-
-
-# On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
-# When running this app on the local machine, default the port to 8000
-port = int(os.getenv('PORT', 8000))
-
-@app.route('/')
-def root():
-    return app.send_static_file('Game_s_Keeper_Login.html')
-
-
-@app.route('/dashboard')
-def dashboard():
-    return app.send_static_file('Game_Keeper_Page')
 
 @atexit.register
 def shutdown():
