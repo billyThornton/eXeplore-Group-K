c3dd459d419df2c9d4572b84d8b444c891eb6021
Billy Thornton <billy.bob.thornton@sky.com>
2020-02-14 10:37:12 +0000

    Basic Files Required to run the server with our basic html framework

---
+++ manage.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, jsonify
+import atexit
+import os
+import json
+
+app = Flask(__name__, static_url_path='')
+
+db_name = 'mydb'
+client = None
+db = None
+
+
+# On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
+# When running this app on the local machine, default the port to 8000
+port = int(os.getenv('PORT', 8000))
+
+@app.route('/')
+def root():
+    return app.send_static_file('Game_s_Keeper_Login.html')
+
+# /* Endpoint to greet and add a new visitor to database.
+# * Send a POST request to localhost:8000/api/visitors with body
+# * {
+# *     "name": "Bob"
+# * }
+# */
+
+@app.route('/dashboard')
+def dashboard():
+    return app.send_static_file('Game_Keeper_Page.html')
+    
+@app.route('/api/visitors', methods=['GET'])
+def get_visitor():
+    if client:
+        return jsonify(list(map(lambda doc: doc['name'], db)))
+    else:
+        print('No database')
+        return jsonify([])
+
+@atexit.register
+def shutdown():
+    if client:
+        client.disconnect()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=True)
