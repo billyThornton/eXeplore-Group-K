30c80826841b9b2879e2cdc440546072099592b4
Billy Thornton <billy.bob.thornton@sky.com>
2020-02-18 18:05:45 +0000

    Update backend to connect to the database Added a function to the manage.py file to test this functionality

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import atexit
 import os
 import json
+import ibm_db
 
 app = Flask(__name__, static_url_path='')
 
@@ -9,10 +10,37 @@ db_name = 'mydb'
 client = None
 db = None
 
+if 'VCAP_SERVICES' in os.environ:
+    db2info = json.loads(os.environ['VCAP_SERVICES'])['dashDB'][0]
+    db2cred = db2info["credentials"]
+    appenv = json.loads(os.environ['VCAP_APPLICATION'])
+else:
+    raise ValueError('Expected cloud environment')
 
 # On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8000
 port = int(os.getenv('PORT', 8000))
+
+def getStudent():
+    db2conn = ibm_db.connect("DATABASE="+db2cred['db']+";HOSTNAME="+db2cred['hostname']+";PORT="+str(db2cred['port'])+";UID="+db2cred['username']+";PWD="+db2cred['password']+";","","")
+    if db2conn:
+        # we have a Db2 connection, query the database
+        sql="select * from STUDENT"
+        # Note that for security reasons we are preparing the statement first,
+        # then bind the form input as value to the statement to replace the
+        # parameter marker.
+        stmt = ibm_db.prepare(db2conn,sql)
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+     return render_template('Game_s_Keeper_Login.html', return=rows)
+
 
 @app.route('/')
 def root():
