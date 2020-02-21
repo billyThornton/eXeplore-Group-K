013af186daeba2a58c0c970be6534b10a8623aaf
Billy Thornton <billy.bob.thornton@sky.com>
2020-02-21 11:29:27 +0000

    Updated to render templates

--- manage.py
+++ manage.py
@@ -58,7 +58,7 @@ def root():
 
 @app.route('/dashboard')
 def dashboard():
-    return app.send_static_file('Game_Keeper_Page.html')
+    return render_template('Game_Keeper_Page.html')
     
 @app.route('/api/visitors', methods=['GET'])
 def get_visitor():
