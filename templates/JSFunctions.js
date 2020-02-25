aa59b3af196921db2a96a0c77daf7d9b8a60eb4b
Ben Trotter <benjamin.trotter@yahoo.co.uk>
2020-02-25 11:17:02 +0000

    Pushing front-end desktop

---
+++ templates/JSFunctions.js
@@ -0,0 +1,23 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+function loadDoc(file)
+{
+  $(document).ready(function()
+  {
+    $('#content').load(file);
+  });
+}
+
+/* side bar nav buttin*/
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
