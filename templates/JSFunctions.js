58a96cb16a56823b58b749fd5cf3613c3f004d4f
Ben Trotter <benjamin.trotter@yahoo.co.uk>
2020-02-25 19:10:59 +0000

    Tidied mobile app. Added alert for unfinished feautures.

--- templates/JSFunctions.js
+++ templates/JSFunctions.js
@@ -21,3 +21,7 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+function appConstruction() {
+	alert("Application currently under construction.\nFeature not added yet.")
+}
