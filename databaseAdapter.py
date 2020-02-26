ec5bcd8d9c607182d51182d3a32a221c79142940
Kristian W <kristian943@hotmail.co.uk>
2020-02-26 16:25:42 +0000

    Updated databaseAdapter.py to have necessary sql queries in separate python functions.

--- databaseAdapter.py
+++ databaseAdapter.py
@@ -6,7 +6,7 @@ import ibm_db
 import requests
 
 app = Flask(__name__)
-localFlag = True 
+localFlag = True
 db_name = 'mydb'
 client = None
 db = None
@@ -16,7 +16,7 @@ if 'VCAP_SERVICES' in os.environ:
     db2info = vcapEnv['dashDB For Transactions'][0]
     db2cred = db2info["credentials"]
     appIDInfo = vcapEnv['AppID'][0]['credentials']
-    
+
     provider_config={
      "issuer": "appid-oauth.ng.bluemix.net",
      "authorization_endpoint": appIDInfo['oauthServerUrl']+"/authorization",
@@ -32,18 +32,34 @@ elif localFlag:
     connectionInfo = ["DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-lon02-02.services.eu-gb.bluemix.net;PORT=50000;UID=xkm27482;PWD=70852r6bqw-s8dgn;", "", ""]
 else:
     raise ValueError('Expected cloud environment')
-	
-def getStudent():
+
+
+def createConnection():
     if localFlag:
         db2conn= ibm_db.connect(connectionInfo[0],connectionInfo[1],connectionInfo[2])
     else:
         db2conn = ibm_db.connect("DATABASE="+db2cred['db']+";HOSTNAME="+db2cred['hostname']+";PORT="+str(db2cred['port'])+";UID="+db2cred['username']+";PWD="+db2cred['password']+";","","")
+
+    return db2conn
+
+
+def getStudent(tutor):
+
+    db2conn = createConnection()
+
     if db2conn:
         # if we have a Db2 connection, query the database
-        sql="select * from locations"
-        #Prepare the statement 
+        sql = (
+        "SELECT s.name, s.email"
+        " FROM student s"
+        " INNER JOIN tutor t"
+        " ON s.tutor_id = t.tutor_id"
+        " WHERE tutor_name = '" + tutor +
+        "';"
+        )
+        # Prepare the statement
         stmt = ibm_db.prepare(db2conn,sql)
-		#Execute the sql
+		# Execute the sql
         ibm_db.execute(stmt)
         rows=[]
         # fetch the result
@@ -53,7 +69,344 @@ def getStudent():
             result = ibm_db.fetch_assoc(stmt)
         # close database connection
         ibm_db.close(db2conn)
-        #Print to screen the result
+        # Print to screen the result
         print(rows)
     return rows
-getStudent()
+
+
+def getTeamMembers(teamName):
+
+    db2conn = createConnection()
+
+    if db2conn:
+        # if we have a Db2 connection, query the database
+        sql = (
+        "SELECT s.name, s.email"
+        " FROM student s"
+        " INNER JOIN team t"
+        " ON s.team_id = t.team_id"
+        " WHERE team_name = '" + teamName +
+        "';"
+        )
+        # Prepare the statement
+        stmt = ibm_db.prepare(db2conn,sql)
+		# Execute the sql
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+        # Print to screen the result
+        print(rows)
+    return rows
+
+
+def getRouteLocations(routeName): #Need to check the SQL - might be confusing route_id and route_name
+
+    db2conn = createConnection()
+
+    if db2conn:
+        # if we have a Db2 connection, query the database
+        sql = (
+        "SELECT l.location_name"
+        " FROM location l"
+        " INNER JOIN route r"
+        " ON l.location_id = r.location_id"
+        " WHERE route_name = '" + routeName +
+        "';"
+        )
+        # Prepare the statement
+        stmt = ibm_db.prepare(db2conn,sql)
+		# Execute the sql
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+        # Print to screen the result
+        print(rows)
+    return rows
+
+
+def getTeamFromStudent(student):
+
+
+    db2conn = createConnection()
+
+    if db2conn:
+        # if we have a Db2 connection, query the database
+        sql = (
+        "SELECT t.team_name, t.progress, r.route_name"
+        " FROM team t"
+        " INNER JOIN student s"
+        " ON t.team_id = s.team_id"
+        " INNER JOIN route r"
+        " ON t.route_id = r.route_id"
+        " WHERE s.name = '" + student +
+        "';"
+        )
+        # Prepare the statement
+        stmt = ibm_db.prepare(db2conn,sql)
+		# Execute the sql
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+        # Print to screen the result
+        print(rows)
+    return rows
+
+def getTeamFromID(teamID):
+
+
+    db2conn = createConnection()
+
+    if db2conn:
+        # if we have a Db2 connection, query the database
+        sql = (
+        "SELECT t.team_name, t.progress, r.route_name"
+        " FROM team t"
+        " INNER JOIN route r"
+        " ON t.route_id = r.route_id"
+        " WHERE t.team_id = " + str(teamID) +
+        ";"
+        )
+        # Prepare the statement
+        stmt = ibm_db.prepare(db2conn,sql)
+		# Execute the sql
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+        # Print to screen the result
+        print(rows)
+    return rows
+
+
+def getStudentProgress(student):
+
+    db2conn = createConnection()
+
+    if db2conn:
+        # if we have a Db2 connection, query the database
+        sql = (
+        "SELECT t.progress"
+        " FROM team t"
+        " INNER JOIN student s"
+        " ON t.team_id = s.team_id"
+        " WHERE name = '" + student +
+        "';"
+        )
+        # Prepare the statement
+        stmt = ibm_db.prepare(db2conn,sql)
+		# Execute the sql
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+        # Print to screen the result
+        print(rows)
+    return rows
+
+
+def getTutorOffice(tutor):
+
+    db2conn = createConnection()
+
+    if db2conn:
+        # if we have a Db2 connection, query the database
+        sql = (
+        "SELECT o.office_name"
+        " FROM office o"
+        " INNER JOIN tutor t"
+        " ON t.office_id = o.office_id"
+        " WHERE tutor_name = '" + tutor +
+        "';"
+        )
+        # Prepare the statement
+        stmt = ibm_db.prepare(db2conn,sql)
+		# Execute the sql
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+        # Print to screen the result
+        print(rows)
+    return rows
+
+
+def getRouteID(teamID):
+
+    db2conn = createConnection()
+
+    if db2conn:
+        # if we have a Db2 connection, query the database
+        sql = (
+        "SELECT route_id"
+        " FROM team"
+        " WHERE team_id = " + str(teamID) +
+        ";"
+        )
+        # Prepare the statement
+        stmt = ibm_db.prepare(db2conn,sql)
+		# Execute the sql
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+        # Print to screen the result
+        print(rows)
+    return rows
+
+
+def getOfficeLocation(officeID):
+
+    db2conn = createConnection()
+
+    if db2conn:
+        # if we have a Db2 connection, query the database
+        sql = (
+        "SELECT office_name, location_id"
+        " FROM office"
+        " WHERE office_id = " + str(officeID) +
+        ";"
+        )
+        # Prepare the statement
+        stmt = ibm_db.prepare(db2conn,sql)
+		# Execute the sql
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+        # Print to screen the result
+        print(rows)
+    return rows
+
+
+def getLocationClues(locationID):
+
+    db2conn = createConnection()
+
+    if db2conn:
+        # if we have a Db2 connection, query the database
+        sql = (
+        "SELECT contents"
+        " FROM clue"
+        " WHERE location_id = " + str(locationID) +
+        ";"
+        )
+        # Prepare the statement
+        stmt = ibm_db.prepare(db2conn,sql)
+		# Execute the sql
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+        # Print to screen the result
+        print(rows)
+    return rows
+
+
+def getStudentPassword(student):
+
+    db2conn = createConnection()
+
+    if db2conn:
+        # if we have a Db2 connection, query the database
+        sql = (
+        "SELECT p.password"
+        " FROM student_password p"
+        " INNER JOIN student s"
+        " ON s.student_id = p.student_id"
+        " WHERE name = '" + student +
+        "';"
+        )
+        # Prepare the statement
+        stmt = ibm_db.prepare(db2conn,sql)
+		# Execute the sql
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+        # Print to screen the result
+        print(rows)
+    return rows
+
+
+def getTutorPassword(tutor):
+
+    db2conn = createConnection()
+
+    if db2conn:
+        # if we have a Db2 connection, query the database
+        sql = (
+        "SELECT p.password"
+        " FROM tutor_password p"
+        " INNER JOIN student s"
+        " ON t.tutor_id = p.tutor_id"
+        " WHERE tutor_name = '" + tutor +
+        "';"
+        )
+        # Prepare the statement
+        stmt = ibm_db.prepare(db2conn,sql)
+		# Execute the sql
+        ibm_db.execute(stmt)
+        rows=[]
+        # fetch the result
+        result = ibm_db.fetch_assoc(stmt)
+        while result != False:
+            rows.append(result.copy())
+            result = ibm_db.fetch_assoc(stmt)
+        # close database connection
+        ibm_db.close(db2conn)
+        # Print to screen the result
+        print(rows)
+    return rows
