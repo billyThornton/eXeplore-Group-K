30c79ba3dd546057f08734a13f55d5b5ee64f0f0
Jamie <jamiebutler960@gmail.com>
2020-03-09 17:09:14 +0000

    added search bars to managelocations

--- static/js/JSFunctions.js
+++ static/js/JSFunctions.js
@@ -296,3 +296,22 @@ function noNumber(str)
 {
   return !(/[0-9]/.test(str));
 }
+
+/**
+* Filters list of locations live for search bar
+*/
+function searchList(inputID, ulID) {
+    var input, filter, ul, li, i, txtValue;
+    input = document.getElementById(inputID);
+    filter = input.value.toUpperCase();
+    ul = document.getElementById(ulID);
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        txtValue = li[i].textContent || li[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
