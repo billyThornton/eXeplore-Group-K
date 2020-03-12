92812e1e18dd86f389848971c0fafa02a1d65ca3
Ben Trotter <benjamin.trotter@yahoo.co.uk>
2020-03-12 00:05:07 +0000

    Google translate and comments

--- static/js/JSFunctions.js
+++ static/js/JSFunctions.js
@@ -1,8 +1,8 @@
 /**
  * Created on 19/02/2020
  * @author: Ben Trotter
- * @Last Edited: 26/02/2020
- * @edited by: Billy Thornton
+ * @Last Edited: 11/03/2020
+ * @edited by: Ben Trotter
  *
  * Copyright (c) “2020, by Group K
  * Contributors: Jamie Butler, Rahul Pankhania, Teo Reed, Billy Thornton, Ben Trotter,
@@ -31,6 +31,7 @@
  * the application.
  */
 
+// Global for reloading screens after form submission
  var currentScreen = "default";
 
 /**
@@ -50,6 +51,11 @@ function loadDoc(file)
   });
 }
 
+/**
+ * @brief Reverts the page back to the dashboard page and sets the global variable
+ * to default.
+ *
+ */
 function clearRedirect()
 {
   localStorage.currentScreen = "default";
@@ -276,32 +282,35 @@ function validateCreate()
 }
 
 /**
-* Checks to see if there isn't an uppercase or lowercase letter in a string.
-*
-* @param {string} Str String to be checked.
-*
-* @return {boolean} true if there is no upper or lower case letter in the string.
-*/
+ * Checks to see if there isn't an uppercase or lowercase letter in a string.
+ *
+ * @param {string} Str String to be checked.
+ *
+ * @return {boolean} true if there is no upper or lower case letter in the string.
+ */
 function noMixedCase(str)
 {
   return !(/[a-z]/.test(str)) || !(/[A-Z]/.test(str));
 }
 
 /**
-* Checks to see if there isn't a number in a string.
-*
-* @param {string} Str String to be checked.
-*
-* @return {boolean} true if there is no number in the string.
-*/
+ * Checks to see if there isn't a number in a string.
+ *
+ * @param {string} Str String to be checked.
+ *
+ * @return {boolean} true if there is no number in the string.
+ */
 function noNumber(str)
 {
   return !(/[0-9]/.test(str));
 }
 
 /**
-* Filters list of locations live for search bar
-*/
+ * Filters list of locations live for search bar
+ * 
+ * @param {int} inputID the search box tag id.
+ * @param {int} ulID the id of the unsorted list.
+ */
 function searchList(inputID, ulID) {
     var input, filter, ul, li, i, txtValue;
     input = document.getElementById(inputID);
